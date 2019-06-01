a48d538a541ec5c9d394b59cba0b7e10f1263694
HeWei <wei.he1@thoughtworks.com>
2019-06-01 13:06:05 +0800

    confused

--- src/module/count/CounterAction.ts
+++ src/module/count/CounterAction.ts
@@ -1,29 +1,16 @@
 // 定义增加 state 类型常量
-export const INCREMENT = "INCREMENT";
-export type INCREMENT = typeof INCREMENT;
+import {createAction} from "redux-actions-helper";
+import {Action} from "redux";
 
-// 定义减少 state 类型常量
-export const DECREMENT = "DECREMENT";
-export type DECREMENT = typeof DECREMENT;
-
-export interface IINCREMENTAction {
-    type: INCREMENT;
-}
-
-export interface IDECREMENTAction {
-    type: DECREMENT;
-}
+export const INCREMENT= "INCREMENT";
+export const DECREMENT= "DECREMENT";
 
 // 定义 modifyAction 类型，包含 IINCREMENTAction 和 IDECREMENTAction 接口类型
-export type ModifyAction = IINCREMENTAction | IDECREMENTAction;
-
+export interface ModifyAction extends Action {
+}
 
 // 增加 state 次数的方法
-export const increment = (): IINCREMENTAction => ({
-    type: INCREMENT,
-});
+export const increment = createAction<ModifyAction>(INCREMENT);
 
 // 减少 state 次数的方法
-export const decrement = (): IDECREMENTAction => ({
-    type: DECREMENT
-});
+export const decrement = createAction(DECREMENT);
