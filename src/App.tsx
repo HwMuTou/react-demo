9f383a39a33a88df33a51ab308863f27fc475425
852224651@qq.com <852224651@qq.com>
2019-07-18 20:55:21 +0800

    some page format

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,39 @@ import Home from "./components/home/Home";
 import Game from "./components/game/Game";
 import CounterContainer from "./containers/CounterContainer";
 import TodoListContainer from "./containers/TodoListContainer";
+import {Container, Menu, Responsive} from "semantic-ui-react";
 
 class App extends React.Component<RouteComponentProps> {
     render() {
         return (
-            <div>
-                <h1>React Router Tutorial</h1>
-                <ul>
-                    <li><NavLink to={`/home`}>Home</NavLink></li>
-                    <li><NavLink to={`/game`}>Game</NavLink></li>
-                    <li><NavLink to={`/counter`}>Counter</NavLink></li>
-                    <li><NavLink to={`/todo`}>TODO</NavLink></li>
-                </ul>
+            <Responsive>
+
+                <Menu>
+                    <Container>
+                        <Menu.Item as={'a'}>
+                            <NavLink to={`/home`}>Home</NavLink>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <NavLink to={`/game`}>Game</NavLink>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <NavLink to={`/counter`}>Counter</NavLink>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <NavLink to={`/todo`}>TODO</NavLink>
+                        </Menu.Item>
+                    </Container>
+                </Menu>
 
                 <Switch>
-                    <Route path={`/home`} component={Home}/>
                     <Route path={`/game`} component={Game}/>
                     <Route path={`/counter`} component={CounterContainer}/>
                     <Route path={`/todo`} component={TodoListContainer}/>
+
+                    <Route component={Home}/>
                 </Switch>
 
-            </div>
+            </Responsive>
         )
     }
 }
