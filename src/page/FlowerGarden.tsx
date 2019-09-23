123e6c9db2c6a4f70e5edeeed7bf4c52bc27aad9
HeWei <wei.he1@thoughtworks.com>
2019-09-23 12:48:14 +0800

    h3 .

--- src/page/FlowerGarden.tsx
+++ src/page/FlowerGarden.tsx
@@ -8,7 +8,19 @@ export class FlowerGarden extends React.Component {
         return (
             <div className={style.page}>
                 <div className={style.header}>
+                    <p>DOWNLOAD THE EXAMPLE HTML FILE AND CSS FILE</p>
+                </div>
 
+                <div className={style.content}>
+                    <div className={style.mainContent}>
+                        <div className={style.paragraph}>
+                            <h3 className={style.preamble}>The Road to Enlightenment</h3>
+                        </div>
+                    </div>
+
+                    <div className={style.sidebarContent}>
+
+                    </div>
                 </div>
             </div>
         )
